e85c2a18c053c9d24d32557c79d9cc400a327e78
Rhys Canady <rhysdaniel064@gmail.com>
2016-05-26 05:58:29 -0500

    ee

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -21,7 +21,39 @@ public class Chess {
     public static char[][] board = new char[8][8];
 
     public static void main(String[] args) {
-        board[2][5] = wknight;
+//        board[2][5] = wknight;
+        board[0][0] = wrook;
+        board[0][7] = wrook;
+        board[0][1] = wknight;
+        board[0][6] = wknight;
+        board[0][2] = wbishop;
+        board[0][5] = wbishop;
+        board[0][3] = wking;
+        board[0][4] = wqueen;
+        board[1][0] = wpawn;
+        board[1][1] = wpawn;
+        board[1][2] = wpawn;
+        board[1][3] = wpawn;
+        board[1][4] = wpawn;
+        board[1][5] = wpawn;
+        board[1][6] = wpawn;
+        board[1][7] = wpawn;
+        board[7][0] = wrook;
+        board[7][7] = wrook;
+        board[7][1] = wknight;
+        board[7][6] = wknight;
+        board[7][2] = wbishop;
+        board[7][5] = wbishop;
+        board[7][3] = wking;
+        board[7][4] = wqueen;
+        board[6][0] = wpawn;
+        board[6][1] = wpawn;
+        board[6][2] = wpawn;
+        board[6][3] = wpawn;
+        board[6][4] = wpawn;
+        board[6][5] = wpawn;
+        board[6][6] = wpawn;
+        board[6][7] = wpawn;
         draw();
     }// end of main
 
@@ -31,7 +63,6 @@ public class Chess {
                 if (j < board[1].length - 1) {
                     if (board[i][j] != wking && board[i][j] != wqueen && board[i][j] != wrook && board[i][j] != wbishop && board[i][j] != wknight && board[i][j] != wpawn
                             && board[i][j] != bking && board[i][j] != bqueen && board[i][j] != brook && board[i][j] != bbishop && board[i][j] != bknight && board[i][j] != bpawn) {
-//                        System.out.print("∶∶");
                         if (j % 2 == 0) {
                             if (i % 2 == 0) {
                                 System.out.print(space1);
@@ -40,7 +71,6 @@ public class Chess {
                                 System.out.print(space2);
                                 System.out.print(space2);
                             }
-//                            System.out.print(space1);
                         } else {
                             if (i % 2 == 0) {
                                 System.out.print(space2);
@@ -66,7 +96,7 @@ public class Chess {
                                 System.out.print(space2);
                                 System.out.print(board[i][j]);
                             } else {
-                                System.out.println(space1);
+                                System.out.print(space1);
                                 System.out.print(board[i][j]);
                                 System.out.print('⃣');
                             }
@@ -100,13 +130,15 @@ public class Chess {
                             System.out.print(board[i][j]);
                             System.out.println('⃣');
                         } else {
-                            System.out.print(board[i][j]);
-                            System.out.println(space2);
+                            System.out.print(space2);
+                            System.out.println(board[i][j]);
+
                         }
                     } else {
                         if (j % 2 == 0) {
-                            System.out.print(board[i][j]);
-                            System.out.println(space2);
+                            System.out.print(space2);
+                            System.out.println(board[i][j]);
+
                         } else {
                             System.out.print(space1);
                             System.out.print(board[i][j]);
---
+++ src/chess/Queen.java
@@ -0,0 +1,51 @@
+package chess;
+
+public class Queen {
+
+    private int x, y;
+    private char symbol;
+    private String color;
+
+    Queen(int a, int b, String d) {
+        this.x = a;
+        this.y = b;
+        this.color = d;
+        if (this.color.equals("white")) {
+            this.symbol = '♕';
+        } else if (this.color.equals("black")) {
+            this.symbol = '♛';
+        }
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setSymbol(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
